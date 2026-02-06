2e64a47fb209032b72b59d7b3e58203fe16ee201
Jin Qing <jinq0123@163.com>
2026-02-06 12:18:10 +0800

    传入的 fd 会在 StopTunnel() 时关闭，读写出错时也会关闭。

--- client/ios/hy/hy_android_fd.go
+++ client/ios/hy/hy_android_fd.go
@@ -6,12 +6,12 @@ import (
 	"os"
 
 	"github.com/xjasonlyu/tun2socks/v2/buffer"
-	"golang.org/x/sys/unix"
 )
 
 // StartTunnelWithAndroidTunFd 启动hysteria隧道，使用Android Tun FD。
 //
 // 仅用于 Android 系统，其他系统请使用 StartTunnel()。
+// 传入的 fd 会在 StopTunnel() 时关闭，读写出错时也会关闭。
 func StartTunnelWithAndroidTunFd(fd int, cfg *HyConfig) (*MogoHysteria, error) {
 	tunFile, err := makeTunFile(fd)
 	if err != nil {
@@ -59,19 +59,11 @@ func (a *androidPacketFlow) close() {
 }
 
 // makeTunFile returns an os.File object from a TUN file descriptor `fd`.
-// The returned os.File holds a separate reference to the underlying file,
-// so the file will not be closed until both `fd` and the os.File are
-// separately closed.  (UNIX only.)
 func makeTunFile(fd int) (*os.File, error) {
 	if fd < 0 {
 		return nil, errors.New("must provide a valid TUN file descriptor")
 	}
-	// Make a copy of `fd` so that os.File's finalizer doesn't close `fd`.
-	newfd, err := unix.Dup(fd)
-	if err != nil {
-		return nil, err
-	}
-	file := os.NewFile(uintptr(newfd), "")
+	file := os.NewFile(uintptr(fd), "")
 	if file == nil {
 		return nil, errors.New("failed to open TUN file descriptor")
 	}
